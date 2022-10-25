ef849eea8369e9a5d00341b88bf194ef27bc23a5
GUSTAVO BRUNO OLIVEIRA VASCONCELOS <115301469+GustavoBOV@users.noreply.github.com>
2022-10-25 19:37:30 -0300

    Add files via upload

---
+++ Semana 3/Exercício05/plot01.py
@@ -0,0 +1,4 @@
+#Matplotlib é uma biblioteca ampla a qual pode criar visualizações animadas, estáticas e interativas em Python.
+#Proporcionando a geração de gráficos com ótima qualidade, a formação de plots interativos que podem ampliar, deslocar e atualizar. 
+#Do mesmo modo é possivel personalizar layout e o design virtual.
+#Nos proporcionando a possibilidade de usar uma vasta variedade de pacotes de terceiros criados no Matplotlib.
